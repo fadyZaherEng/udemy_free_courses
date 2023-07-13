4f048cb84ff00766b4386200dc240eed805f5b9e
fadyZaherEng <fady33zaher@gmail.com>
2023-07-13 19:35:58 +0300

    upload 20

--- lib/layout/home/home_screen.dart
+++ lib/layout/home/home_screen.dart
@@ -13,125 +13,120 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<UdemyCubit>(
-      create: (ctx)=>UdemyCubit()..getCategories()..getCourse(),
-      child: BlocConsumer<UdemyCubit,UdemyAppSates>(
-        listener: (context,state){},
-        builder: (ctx,state){
-          return Scaffold(
+    return BlocConsumer<UdemyCubit,UdemyAppSates>(
+      listener: (context,state){},
+      builder: (ctx,state){
+        return Scaffold(
+          backgroundColor: Color(0xffFFFFFF),
+          appBar: AppBar(
+            elevation: 0,
             backgroundColor: Color(0xffFFFFFF),
-            appBar: AppBar(
-              elevation: 0,
-              backgroundColor: Color(0xffFFFFFF),
-              flexibleSpace: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Hello",
-                            style: GoogleFonts.notoSans(
-                                fontSize: 16,
-                                color: Color(0XFF747474),
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            "Find your Free Courses",
-                            style: GoogleFonts.notoSans(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xff232323)),
-                          ),
-                        ],
-                      ),
-                      SvgPicture.asset("assets/images/love.svg",color: Color(0xff99CAE1),),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            body: (state is InitialCoursesUdemyAppSates||state is ErrorCourseUdemyAppSates)
-                ? Center(
-              child: Container(
-                child: SpinKitWave(
-                  color: Color(0xff99CAE1),
-                  size: 30.0,
-                ),
-              ),
-              ////categories
-            )
-                : SingleChildScrollView(
+            flexibleSpace: SafeArea(
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 12),
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        "Category",
-                        style: GoogleFonts.notoSans(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff232323)),
-                      ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hello",
+                          style: GoogleFonts.notoSans(
+                              fontSize: 16,
+                              color: Color(0XFF747474),
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Find your Free Courses",
+                          style: GoogleFonts.notoSans(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff232323)),
+                        ),
+                      ],
                     ),
-                    Container(
-                      padding: EdgeInsets.only(
-                        top: 40,
-                      ),
-                      width: double.infinity,
-                      height: 200,
-                      child: ListView.builder(
-                          itemCount: UdemyCubit.get(context).cats.length,
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (ctx, int index) {
-                            return CategoryTile(
-                              imageURL: UdemyCubit.get(context).cats[index].imageURL,
-                              color: UdemyCubit.get(context).cats[index].color,
-                              categoryName: UdemyCubit.get(context).cats[index].categoryName,
-                            );
-                          }),
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 12),
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        "Courses",
-                        style: GoogleFonts.notoSans(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff232323)),
-                      ),
-                    ),
-                    Container(
-                      child: ListView.builder(
-                          itemCount: UdemyCubit.get(context).courses.length,
-                          physics: ClampingScrollPhysics(),
-                          shrinkWrap: true,
-                          itemBuilder: (ctx, int index) {
-                            return CourseTile(
-                              imageURL: UdemyCubit.get(context).courses[index].image,
-                              title: UdemyCubit.get(context).courses[index].heading,
-                              courseURL: UdemyCubit.get(context).courses[index].courseLink,
-                              successRate: '',
-
-                            );
-                          }),
-                    ),
+                    SvgPicture.asset("assets/images/love.svg",color: Color(0xff99CAE1),),
                   ],
                 ),
               ),
             ),
-          );
-        },
-      ),
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Category",
+                      style: GoogleFonts.notoSans(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff232323)),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: 40,
+                    ),
+                    width: double.infinity,
+                    height: 200,
+                    child: ListView.builder(
+                        itemCount: UdemyCubit.get(context).category.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (ctx, int index) {
+                          return CategoryTile(
+                            imageURL: UdemyCubit.get(context).category[index].imageURL,
+                            color: UdemyCubit.get(context).category[index].color,
+                            categoryName: UdemyCubit.get(context).category[index].categoryName,
+                          );
+                        }),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Courses",
+                      style: GoogleFonts.notoSans(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff232323)),
+                    ),
+                  ),
+                  (state is InitialCoursesUdemyAppSates||state is ErrorCourseUdemyAppSates) ?
+                  Center(
+                    child: Container(
+                      child: SpinKitWave(
+                        color: Color(0xff99CAE1),
+                        size: 30.0,
+                      ),
+                    ),
+                    ////categories
+                  ):Container(child: ListView.builder(
+                      itemCount: UdemyCubit.get(context).courses.length,
+                      physics: ClampingScrollPhysics(),
+                      shrinkWrap: true,
+                      itemBuilder: (ctx, int index) {
+                        return CourseTile(
+                          imageURL: UdemyCubit.get(context).courses[index].image,
+                          title: UdemyCubit.get(context).courses[index].heading,
+                          courseURL: UdemyCubit.get(context).courses[index].courseLink,
+                          successRate: '',
+
+                        );
+                      }),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:udemy_free_courses/bloc_observer/observer.dart';
+import 'package:udemy_free_courses/layout/home/cubit/cubit.dart';
+import 'package:udemy_free_courses/layout/home/cubit/states.dart';
 import 'package:udemy_free_courses/modules/welcome_screen/welcome.dart';
 import 'package:udemy_free_courses/shared/network/local/cashe_helper.dart';
+import 'package:udemy_free_courses/shared/network/remote/dio_helper.dart';
 import 'package:udemy_free_courses/shared/styles/themes.dart';
 
 
@@ -10,19 +14,28 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = SimpleBlocObserver();
   await SharedHelper.init();
+  await DioHelper.Init();
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme(),
-      darkTheme: darkTheme(),
-      themeMode: ThemeMode.system,
-      title: "udemy free corse app",
-      home: WelcomeScreen(),
+    return BlocProvider<UdemyCubit>(
+      create:(ctx)=>UdemyCubit()..getCategories()..getCourse() ,
+      child: BlocConsumer<UdemyCubit,UdemyAppSates>(
+        listener: (context,state){},
+        builder: (context,state){
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: lightTheme(),
+            darkTheme: darkTheme(),
+            themeMode: ThemeMode.system,
+            title: "udemy free corse app",
+            home: WelcomeScreen(),
+          );
+        },
+      )
     );
   }
 }
